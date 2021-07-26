ead1ee64a17d77e5d56d5127d1333541ad01ae0f
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-26 20:07:12 +0530

    :art: style(Displaying Contacts)
    - Conatct UI
    
    - Hardcoded contacts are now visible on the UI

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,20 @@
+import React, { useContext, Fragment } from 'react'
+
+import ContactContext from '../../context/contact/contactContext'
+
+// Import components
+import ContactItem from './ContactItem'
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext)
+  const { contacts } = contactContext
+  return (
+    <Fragment>
+      {contacts.map((contact) => (
+        <ContactItem contact={contact} key={contact.id} />
+      ))}
+    </Fragment>
+  )
+}
+
+export default Contacts
